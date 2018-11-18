65fbe7fa59985c4b455c6901a2bf1204f007aa78
Charles Saracco <crsaracco@gmail.com>
2018-11-17 23:55:08 -0500

    Move function type definitions and function implementations to a new submodule for now

---
+++ src/function.rs
@@ -0,0 +1,93 @@
+use std::os::raw::c_void;
+
+
+/// Host dispatch function:
+/// Called by the plugin to request something from the host.
+pub type HostDispatch = fn(
+    effect: *mut crate::AEffect,
+    opcode: i32,
+    index: i32,
+    value: isize,
+    ptr: *mut c_void,
+    opt: f32,
+) -> isize;
+
+
+/// Plugin dispatch function:
+/// Called by the host to request something from the plugin.
+pub type PluginDispatch = fn(
+    effect: *mut crate::AEffect,
+    opcode: i32,
+    index: i32,
+    value: isize,
+    ptr: *mut c_void,
+    opt: f32,
+) -> isize;
+
+pub fn plugin_dispatch(_effect: *mut crate::AEffect, _opcode: i32, _index: i32, _value: isize, _ptr: *mut c_void, _opt: f32) -> isize {
+    // Nothing.
+    return 0;
+}
+
+
+/// Set parameter function:
+/// Called by the host to request the plugin to set a parameter to a given value.
+pub type SetParameter = fn(
+    effect: *mut crate::AEffect,
+    index: i32,
+    value: f32,
+);
+
+pub fn set_parameter(_effect: *mut crate::AEffect, _index: i32, _value: f32) {
+    // Nothing.
+}
+
+
+/// Get parameter function:
+/// Called by the host to get the value of a parameter from the plugin.
+pub type GetParameter = fn(
+    effect: *mut crate::AEffect,
+    index: i32,
+) -> f32;
+
+pub fn get_parameter(_effect: *mut crate::AEffect, _index: i32) -> f32 {
+    // Nothing.
+    return 0.0;
+}
+
+
+/// Process function:
+/// Called by the host to get the plugin to process a chunk of samples.
+/// (f32 version)
+pub type Process = fn(
+    effect: *mut crate::AEffect,
+    inputs: *const *const f32,
+    outputs: *mut *mut f32,
+    sample_frames: i32,
+);
+
+pub fn process(_effect: *mut crate::AEffect, _raw_inputs: *const *const f32, _raw_outputs: *mut *mut f32, _samples: i32) {
+    // Nothing.
+}
+
+pub fn process_deprecated(_effect: *mut crate::AEffect, _raw_inputs: *const *const f32, _raw_outputs: *mut *mut f32, _samples: i32) {
+    // This function used to take samples, calculate something based on those samples, and
+    // *add* the result to the original sample (for some reason...?)
+    // It is now DEPRECATED, so it shouldn't ever be called.
+    // Hosts should always call `process()` or `process_f64()` instead.
+}
+
+
+/// Process function:
+/// Called by the host to get the plugin to process a chunk of samples.
+/// (f64 version)
+pub type ProcessF64 = fn(
+    effect: *mut crate::AEffect,
+    inputs: *const *const f64,
+    outputs: *mut *mut f64,
+    sample_frames: i32
+);
+
+pub fn process_f64(_effect: *mut crate::AEffect, _raw_inputs: *const *const f64, _raw_outputs: *mut *mut f64, _samples: i32) {
+    // Nothing.
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,47 @@
 use std::os::raw::c_void;
 
-pub type HostCallbackProc = fn(effect: *mut AEffect, opcode: i32, index: i32, value: isize, ptr: *mut c_void, opt: f32) -> isize;
-pub type DispatcherProc = fn(effect: *mut AEffect, opcode: i32, index: i32, value: isize, ptr: *mut c_void, opt: f32) -> isize;
-pub type SetParameterProc = fn(effect: *mut AEffect, index: i32, parameter: f32);
-pub type GetParameterProc = fn(effect: *mut AEffect, index: i32) -> f32;
-pub type ProcessProc = fn(effect: *mut AEffect, inputs: *const *const f32, outputs: *mut *mut f32, sample_frames: i32);
-pub type ProcessProcF64 = fn(effect: *mut AEffect, inputs: *const *const f64, outputs: *mut *mut f64, sample_frames: i32);
+mod function;
 
 /// Used with the VST API to pass around plugin information.
 #[allow(non_snake_case)]
 #[repr(C)]
 pub struct AEffect {
-    pub magic: i32,                          // Magic number. Must be 0x56737450 ("VstP")
-    pub dispatcher: DispatcherProc,          // Host to plug-in dispatcher.
-    pub _process: ProcessProc,               // DEPRECATED. (Accumulating process mode process() function)
-    pub setParameter: SetParameterProc,      // Set the value of an automatable parameter.
-    pub getParameter: GetParameterProc,      // Get the value of an automatable parameter.
-    pub numPrograms: i32,                    // Number of "programs" (presets)
-    pub numParams: i32,                      // Number of parameters. All presets are assumed to have this many parameters.
-    pub numInputs: i32,                      // Number of audio inputs.
-    pub numOutputs: i32,                     // Number of audio outputs.
-    pub flags: i32,                          // Bitmask. TODO: make flags to put here
-    pub reserved1: isize,                    // Reserved for host. Must be set to 0.
-    pub reserved2: isize,                    // Reserved for host. Must be set to 0.
-    pub initialDelay: i32,                   // Initial sample delay. TODO: document better
-    pub _realQualities: i32,                 // DEPRECATED.
-    pub _offQualities: i32,                  // DEPRECATED.
-    pub _ioRatio: f32,                       // DEPRECATED.
-    pub object: *mut c_void,                 // Void pointer usable by the API to store object data. (???)
-    pub user: *mut c_void,                   // User-defined pointer. (???)
-    pub uniqueId: i32,                       // Unique identifier for the VST. Used during save/load of preset and project.
-    pub version: i32,                        // Plugin version.
-    pub processReplacing: ProcessProc,       // process() function (f32)
-    pub processReplacingF64: ProcessProcF64, // process() function (f64)
-    pub future: [u8; 56],                    // Reserved for future use (should be set to 0).
+    pub magic: i32,                                // Magic number. Must be 0x56737450 ("VstP")
+    pub dispatcher: function::PluginDispatch,      // Host to plug-in dispatcher.
+    pub _process: function::Process,               // DEPRECATED. (Accumulating process mode process() function)
+    pub setParameter: function::SetParameter,      // Set the value of an automatable parameter.
+    pub getParameter: function::GetParameter,      // Get the value of an automatable parameter.
+    pub numPrograms: i32,                          // Number of "programs" (presets)
+    pub numParams: i32,                            // Number of parameters. All presets are assumed to have this many parameters.
+    pub numInputs: i32,                            // Number of audio inputs.
+    pub numOutputs: i32,                           // Number of audio outputs.
+    pub flags: i32,                                // Bitmask. TODO: make flags to put here
+    pub reserved1: isize,                          // Reserved for host. Must be set to 0.
+    pub reserved2: isize,                          // Reserved for host. Must be set to 0.
+    pub initialDelay: i32,                         // Initial sample delay. TODO: document better
+    pub _realQualities: i32,                       // DEPRECATED.
+    pub _offQualities: i32,                        // DEPRECATED.
+    pub _ioRatio: f32,                             // DEPRECATED.
+    pub object: *mut c_void,                       // Void pointer usable by the API to store object data. (???)
+    pub user: *mut c_void,                         // User-defined pointer. (???)
+    pub uniqueId: i32,                             // Unique identifier for the VST. Used during save/load of preset and project.
+    pub version: i32,                              // Plugin version.
+    pub processReplacing: function::Process,       // process() function (f32)
+    pub processReplacingF64: function::ProcessF64, // process() function (f64)
+    pub future: [u8; 56],                          // Reserved for future use (should be set to 0).
 }
 
 #[no_mangle]
-pub extern "C" fn VSTPluginMain(_callback: HostCallbackProc) -> *mut AEffect {
+pub extern "C" fn VSTPluginMain(_callback: function::HostDispatch) -> *mut AEffect {
     // TODO: save the HostCallbackProc for later so that the plugin can call it.
     // Need an actual Plugin struct for this though.
 
-    let plugin = AEffect {
+    let effect = AEffect {
         magic: 0x56737450, // shia_labeouf_magic.gif
-        dispatcher: dispatch,
-        _process: process_deprecated,
-        setParameter: set_parameter,
-        getParameter: get_parameter,
+        dispatcher: function::plugin_dispatch,
+        _process: function::process_deprecated,
+        setParameter: function::set_parameter,
+        getParameter: function::get_parameter,
         numPrograms: 1,
         numParams: 0,
         numInputs: 2, // Stereo input
@@ -62,36 +57,10 @@ pub extern "C" fn VSTPluginMain(_callback: HostCallbackProc) -> *mut AEffect {
         user: Box::into_raw(Box::new(())) as *mut c_void,
         uniqueId: 13371337, // Some random VST ID
         version: 1, // Version 0.0.0.1
-        processReplacing: process_replacing,
-        processReplacingF64: process_replacing_f64,
+        processReplacing: function::process,
+        processReplacingF64: function::process_f64,
         future: [0u8; 56],
     };
 
-    Box::into_raw(Box::new(plugin))
-}
-
-pub fn dispatch(_effect: *mut AEffect, _opcode: i32, _index: i32, _value: isize, _ptr: *mut c_void, _opt: f32) -> isize {
-    // Nothing.
-    return 0;
-}
-
-pub fn process_deprecated(_effect: *mut AEffect, _raw_inputs: *const *const f32, _raw_outputs: *mut *mut f32, _samples: i32) {
-    // Nothing.
-}
-
-pub fn set_parameter(_effect: *mut AEffect, _index: i32, _value: f32) {
-    // Nothing.
-}
-
-pub fn get_parameter(_effect: *mut AEffect, _index: i32) -> f32 {
-    // Nothing.
-    return 0.0;
-}
-
-pub fn process_replacing(_effect: *mut AEffect, _raw_inputs: *const *const f32, _raw_outputs: *mut *mut f32, _samples: i32) {
-    // Nothing.
-}
-
-pub fn process_replacing_f64(_effect: *mut AEffect, _raw_inputs: *const *const f64, _raw_outputs: *mut *mut f64, _samples: i32) {
-    // Nothing.
+    Box::into_raw(Box::new(effect))
 }
